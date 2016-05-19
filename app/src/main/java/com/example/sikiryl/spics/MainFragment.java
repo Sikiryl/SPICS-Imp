d5ab011c5d896e6a3243b44c305ceffeee7d874d
User <artkrub7@gmail.com>
2016-05-19 16:59:20 +0700

    Move database access part to fragment.

--- app/src/main/java/com/example/sikiryl/spics/MainFragment.java
+++ app/src/main/java/com/example/sikiryl/spics/MainFragment.java
@@ -25,14 +25,42 @@ public class MainFragment extends Fragment{
         // Required empty public constructor
     }
 
-
+    private DBHelper helper;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
+        helper = new DBHelper(getActivity());
+        try{
+            Cursor cursor = getProduct();
+            StringBuilder builder = listProduct(cursor);
+            TextView output = (TextView) rootView.findViewById(R.id.productList);
+            output.setText(builder);
+        }
+        finally{
+            helper.close();
+        }
         // Inflate the layout for this fragment
         return rootView;
+
+    }
+    private Cursor getProduct(){
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM product ORDER BY productID;",null);
+        return cursor;
+    }
+
+    private StringBuilder listProduct(Cursor cursor){
+        StringBuilder builder = new StringBuilder("Product list \n\n");
+        while (cursor.moveToNext()){
+            int productID = cursor.getInt(0);
+            String productName = cursor.getString(1);
+            int productPrice = cursor.getInt(2);
+            String productImage = cursor.getString(3);
+            builder.append(productID).append(" ").append(productName).append(" ").append(productPrice).append(" ").append(productImage).append("\n");
+
+        }
+        return builder;
     }
 
 }
